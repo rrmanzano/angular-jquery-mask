3ba9ddd4b84b68ced3160be221df4552bfc7c6ba
Ricardo Ruvalcaba <rrmanzano@hotmail.com>
2017-09-26 21:43:53 -0500

    Removed obsolete code

--- gulpfile.js
+++ gulpfile.js
@@ -45,68 +45,9 @@ gulp.task('concat-header', ['minify'], function() {
 			   .pipe(gulp.dest('./dist/'));
 });
 
-
 gulp.task('default', ['concat-header']);
 
 gulp.task('build', ['concat-header']);
-
-// gulp.task('clean-dist', function() {
-// 	return gulp.src(['./dist/*.*'], {read: false})
-// 			   .pipe(clean());
-// });
-
-// gulp.task('clean-lib', function() {
-// 	return gulp.src(['./lib/*.*'], {read: false})
-// 			   .pipe(clean());
-// });
-
-// gulp.task('compile', ['clean-dist', 'clean-lib'], function () {
-//     return gulp.src('src/**/*.ts')
-//         .pipe(ts({
-//             noImplicitAny: true,
-//             outFile: 'jquery.mask.angular.js'
-//         }))
-//         .pipe(gulp.dest('dist'));
-// });
-
-// gulp.task('concat', function() {
-// 	return gulp.src(['./lib/*.js'])
-// 			   .pipe(concat('jquery.mask.angular.js'))
-// 			   .pipe(gulp.dest('./dist/'));
-// });
-
-// gulp.task('strip-comments', ['concat'], function() {
-// 	return gulp.src(['./dist/*.js'])
-// 			   .pipe(strip())
-// 			   .pipe(gulp.dest('./dist/'));
-// });
-
-// gulp.task('minify', ['strip-comments'], function() {
-// 	return gulp.src('./dist/*.js')
-// 			    .pipe(minify({
-// 			        ext:{
-// 			            min:'.min.js'
-// 			        }
-// 			    }))
-// 		    	.pipe(gulp.dest('./dist/'));
-// });
-
-// gulp.task('concat-header', ['minify'], function() {
-// 	gulp.src(['./header.txt', './dist/*angular.js'])
-// 			   .pipe(concat('jquery.mask.angular.js'))
-// 			   .pipe(gulp.dest('./dist/'));
-
-// 	return gulp.src(['./header.txt', './dist/*angular.min.js'])
-// 			   .pipe(concat('jquery.mask.angular.min.js'))
-// 			   .pipe(gulp.dest('./dist/'));
-// });
-
-
-// gulp.task('default', ['concat-header']);
-
-// gulp.task('build', ['concat-header']);
-
-// gulp.task('clean-all', ['clean-lib', 'clean-dist'], function() { });
 
 // # Install gulp globally
 //		$ npm install --global gulp-cli
